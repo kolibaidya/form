15ee974bde6f674eef0a9c1ad109c9994fbaee37
Koli Baidya <koliranibaidya@gmail.com>
2025-12-23 04:29:33 -0800

    fix: remove accidentally merged phone page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import {
 import LoginPage from "./features/auth/pages/loginPage";
 import RegisterPage from "./features/auth/pages/registerPage";
 import { ProductPage } from "./features/products/pages/productPage";
-import { PhonePage } from "./pages/phonePage";
 
 export default function App() {
   return (
@@ -17,7 +16,6 @@ export default function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/products" element={<ProductPage />} />
-        <Route path="/phones" element={<PhonePage />} />
       </Routes>
     </Router>
   );
--- src/models/phone.tsx
+++
@@ -1,6 +0,0 @@
-export interface Phone {
-  _id: string;
-  Brand: string;
-  Name: string;
-  ReleaseDate: string;
-}
