d5d59dfbff0ee84559fed5efad3ed7de526696e1
Onik <18009030+Onik97@users.noreply.github.com>
2026-03-30 21:35:34 +0100

    Loading clean up (#1)
    * Changed to displays
    
    * Renamed to non capital letter

--- src/pages/phonePage.tsx
+++ src/pages/phonePage.tsx
@@ -1,7 +1,7 @@
 import { CreatePhoneDialog } from "@/components/dialogs/CreatePhoneDialog";
 import ErrorDisplay from "@/components/ErrorDisplay";
 import { PhoneFeed } from "@/components/feed/phoneFeed";
-import LoadingDisplay from "@/components/LoadingDisplay";
+import LoadingDisplay from "@/components/loadingDisplay";
 import { PhoneTable } from "@/components/table/phoneTable";
 import { Card, CardContent } from "@/components/ui/card";
 import { useFetchPhones } from "@/hooks/phoneHooks";
